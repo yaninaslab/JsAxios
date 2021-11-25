9ea30fa9240e70471548bcf3199b72fc08e8b1b7
yaninaslab <yana.belash@gmail.com>
2021-11-25 18:13:24 -0500

    Ended with everything except the bonus

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
 
-function click_button () {
+function click_button() {
     document.getElementById("button_1").style.backgroundColor = "pink";
-    
-    axios.request ({
+
+    axios.request({
         url: "http://www.boredapi.com/api/activity/"
     }).then(bored_success).catch(bored_failed);
 }
@@ -22,35 +22,20 @@ function bored_failed(error) {
 
 var button_1 = document.getElementById("button_1");
 button_1.addEventListener('click', click_button);
-button_3.addEventListener('click', select_part);
+button_4.addEventListener('click', select_part);
 
 
-function select_part(){
-    var select_button = document.getElementById("button_3");
-    document.getElementById("button_3").style.backgroundColor = "pink";
-    axios.request ({
-        url: "http://www.boredapi.com/api/activity?participants=1",
-        method: "GET",
+function select_part() {
+
+    document.getElementById("button_4").style.backgroundColor = "pink";
+    var selected_num = document.getElementById("mySelect").value;
+    axios.get("http://www.boredapi.com/api/activity", {
         params: {
-            participants: 1
-        },
-        url: "http://www.boredapi.com/api/activity?participants=2",
-        method: "GET",
-        params: {
-            participants: 2
-        },
-        url: "http://www.boredapi.com/api/activity?participants=3",
-        method: "GET",
-        params: {
-            participants: 3
-        },
-        url: "http://www.boredapi.com/api/activity?participants=4",
-        method: "GET",
-        params: {
-            participants: 4
+            participants: selected_num,
         }
+    })
 
-    }).then(bored_success).catch(bored_failed);
+        .then(bored_success).catch(bored_failed);
 
 }
 
